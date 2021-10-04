aaa04a65f973948b097a0869eaa093560b9d2fd1
AlexWambui <alexwambuik@gmail.com>
2021-10-04 13:58:50 -0700

    finished adding all prototypes including those for the extra functions

--- main.h
+++ main.h
@@ -4,12 +4,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
+
 /**
-  * struct convert - defines a structure for symbols and functions
-  * 
-  * @sym: The operator
-  * @f: the function associated
-  */
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
 struct convert
 {
 	char *sym;
@@ -17,6 +18,7 @@ struct convert
 };
 typedef struct convert conver_t;
 
+/*Main functions*/
 int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
 int _write_char(char);
@@ -26,5 +28,19 @@ int print_percent(va_list);
 int print_integer(va_list);
 int print_number(va_list);
 int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
+
+/*extra functions*/
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsgined_number(unsigned int);
 
 #endif
+
